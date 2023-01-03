e2545b76aa75a3ab8bc023823d8c2472c3e2b885
FrancoJuri <83733316+FrancoJuri@users.noreply.github.com>
2023-01-03 04:17:40 -0300

    Update firebase-config.js

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -4,12 +4,12 @@ import 'firebase/auth';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCzGYWP_744LEvr4FIVsLzX5Co91euQNz0",
-    authDomain: "fake-store-app-3ef27.firebaseapp.com",
-    projectId: "fake-store-app-3ef27",
-    storageBucket: "fake-store-app-3ef27.appspot.com",
-    messagingSenderId: "372425986762",
-    appId: "1:372425986762:web:e79d2e66b6a78232979a33"
+  apiKey: "AIzaSyCzGYWP_744LEvr4FIVsLzX5Co91euQNz0",
+  authDomain: "fake-store-app-3ef27.firebaseapp.com",
+  projectId: "fake-store-app-3ef27",
+  storageBucket: "fake-store-app-3ef27.appspot.com",
+  messagingSenderId: "372425986762",
+  appId: "1:372425986762:web:e79d2e66b6a78232979a33"
 };
 
 firebase.initializeApp(firebaseConfig);
